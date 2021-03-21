bd335f97da99e04f167a63757e3757b2f6fa416c
Michael J Harvey <michael4d45@gmail.com>
2021-03-21 12:10:19 -0600

    scroll down on unlock

--- resources/js/controls.js
+++ resources/js/controls.js
@@ -2,6 +2,8 @@ import { PointerLockControls } from './PointerLockControls'
 
 const menu = document.querySelector('#menu');
 const play = document.querySelector('#play');
+const nav = document.querySelector('nav');
+const scrollHeight = nav.scrollHeight;
 
 let controls;
 
@@ -13,10 +15,14 @@ export function getControls(camera) {
     });
     controls.addEventListener('lock', function () {
         menu.style.display = 'none';
+        nav.style.display = 'none';
     });
     controls.addEventListener('unlock', function () {
         menu.style.display = '';
+        nav.style.display = '';
+        document.body.scrollTop = scrollHeight;
+        document.documentElement.scrollTop = scrollHeight;
     });
-    
+
     return controls;
 }
